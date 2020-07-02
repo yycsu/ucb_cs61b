88482b82297069cfa1a4510af3f3c0f9f501eeb3
yycsu <yaoyi_csu@163.com>
2020-07-02 15:57:29 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,14 @@
+public class HelloNumbers
+{
+	public static void main(String[] arg)
+	{
+		int value = 0;
+		int x = 0;
+        	while (x < 10) 
+		{
+			value += x;
+            		System.out.print(value + " ");
+			x += 1;
+		}	
+	}
+}
