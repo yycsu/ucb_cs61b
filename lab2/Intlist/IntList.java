e726cb92825dcb3f201abb81fdefa9c11f3229b5
yycsu <yaoyi_csu@163.com>
2020-07-03 11:30:21 +0800

    completed all the work of lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,16 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null)
+            return null;
+
+        if (A.rest == null)
+        {
+            A.rest = B;
+            return A;
+        }
+
+        return new IntList(A.first, dcatenate(A.rest, B));
     }
 
     /**
@@ -91,7 +100,19 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null)
+            return null;
+        IntList res = new IntList(A.first, null);
+        IntList ptr = res;
+
+        A = A.rest;
+        while (A != null) {
+            ptr.rest = new IntList(A.first, null);
+            A = A.rest;
+            ptr = ptr.rest;
+        }
+        ptr.rest = B;
+        return res;
     }
 
 
