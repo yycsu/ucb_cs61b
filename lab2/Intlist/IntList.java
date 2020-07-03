52b211e8341688c8aafc48335bbd9f67bb80c78d
yycsu <yaoyi_csu@163.com>
2020-07-03 11:41:50 +0800

    modified the IntList.java in Lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -83,7 +83,7 @@ public class IntList {
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
         if (A == null)
-            return null;
+            return B;
 
         if (A.rest == null)
         {
@@ -101,7 +101,7 @@ public class IntList {
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
         if (A == null)
-            return null;
+            return B;
         IntList res = new IntList(A.first, null);
         IntList ptr = res;
 
