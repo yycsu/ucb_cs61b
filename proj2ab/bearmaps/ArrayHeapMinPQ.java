499a2475a2ab0392526b41d2aa359aca1f96ab98
yycsu <yaoyi_csu@163.com>
2020-10-12 19:58:27 +0800

    add a HashMap in ArrayHeapMinPQ.java

--- proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -1,5 +1,6 @@
 package bearmaps;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 import org.junit.Test;
@@ -8,10 +9,12 @@ import static org.junit.Assert.assertEquals;
 
 public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
     private ArrayList<PriorityNode> items;
+    private HashMap<T , Integer> IndexMap;
     private int size;
 
     public ArrayHeapMinPQ() {
         items = new ArrayList<>();
+        IndexMap = new HashMap<>();
         items.set(0,null);
         size = 0;
     }
@@ -36,6 +39,9 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
         PriorityNode q = items.get(b);
         items.set(a, q);
         items.set(b, p);
+
+        IndexMap.put(q.item, a);
+        IndexMap.put(p.item, b);
     }
 
     public void swim(int k){
@@ -58,21 +64,22 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
     @Override
     public void add(T item, double priority) {
         items.set(++size, new PriorityNode(item, priority));
+        IndexMap.put(item, size -1);
         swim(size);
+
     }
 
     @Override
     public boolean contains(T item) {
-        int index = items.indexOf(item);
-        if (index == -1){
+        if (size==0){
             return false;
         }
-        return true;
+        return IndexMap.containsKey(item);
     }
 
     @Override
     public T getSmallest() {
-        if (size() == 0) {
+        if (size== 0) {
             throw new NoSuchElementException("PQ is empty");
         }
         return items.get(1).item;
@@ -80,11 +87,11 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
 
     @Override
     public T removeSmallest() {
-
         if (size == 0) {
             throw new NoSuchElementException("PQ is empty");
         }
         T p = items.get(1).item;
+        IndexMap.remove(p);
         swap(1, size--);
         sink(1);
         items.set((size+1),null);
@@ -102,14 +109,17 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
         if (contains(item) == false) {
             throw new NoSuchElementException("PQ does not contain " + item);
         }
-
-        int index = items.indexOf(item);
+        int index = IndexMap.get(item);
+        double oldPriority = items.get(index).priority;
         items.set(index, new PriorityNode(item, priority));
+        if (oldPriority > priority){
+            sink(index);
+        }
         swim(index);
-        sink(1);
     }
 
-    private class PriorityNode implements Comparable<PriorityNode> {
+//    private class PriorityNode implements Comparable<PriorityNode> {
+    private class PriorityNode {
         private T item;
         private double priority;
 
@@ -130,27 +140,27 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T>{
             this.priority = priority;
         }
 
-        @Override
-        public int compareTo(PriorityNode other) {
-            if (other == null) {
-                return -1;
-            }
-            return Double.compare(this.getPriority(), other.getPriority());
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public boolean equals(Object o) {
-            if (o == null || o.getClass() != this.getClass()) {
-                return false;
-            } else {
-                return ((PriorityNode) o).getItem().equals(getItem());
-            }
-        }
-
-        @Override
-        public int hashCode() {
-            return item.hashCode();
-        }
+//        @Override
+//        public int compareTo(PriorityNode other) {
+//            if (other == null) {
+//                return -1;
+//            }
+//            return Double.compare(this.getPriority(), other.getPriority());
+//        }
+//
+//        @Override
+//        @SuppressWarnings("unchecked")
+//        public boolean equals(Object o) {
+//            if (o == null || o.getClass() != this.getClass()) {
+//                return false;
+//            } else {
+//                return ((PriorityNode) o).getItem().equals(getItem());
+//            }
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return item.hashCode();
+//        }
     }
 }
